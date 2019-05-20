a8b533c4ef2a547bbc95e079a1cbd0f24392f3fe
Eduard Shahnazaryan <squared1994@gmail.com>
2019-05-21 02:21:24 +0400

    Use PostgreSQL as the database

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,12 +4,13 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "Eduard Shahnazaryan's Vapor Server"
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
     }
 
     router.post("api", "acronyms") { req -> Future<Acronym> in
