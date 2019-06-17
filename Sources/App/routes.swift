02e768a8f1c0a5d1327accb5c69e0538086b964d
Eduard Shahnazaryan <squared1994@gmail.com>
2019-06-18 00:29:13 +0400

    Adding API Credentials

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,13 +5,23 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "Eduard Shahnazaryan's Vapor Server"
+        return "Eduard Shahnazaryan's Server"
     }
     
     // Basic "Hello, world!" example
     router.get("hello", String.parameter) { req -> String in
         let name = try req.parameters.next(String.self)
         return "Hello, \(name)!"
+    }
+    
+    // API Credentials
+    router.get("api") { req -> String in
+        return """
+[
+    { "name": "Քոչարի" }, { "name": "Լորկե" }, { "name": "Ուզունդարա" }, { "name": "Ֆնջան" }, { "name": "Գորանի" }, { "name": "Լուտկի" }, { "name": "Քերծի" }, { "name": "Էնզելի" }, { "name": "Վերվերի" }, { "name": "Թարս Պար" }, { "name": "Ռոստամ Բազի" }, { "name": "Բերդ" }, { "name": "Սասնա Քոչարի" }, { "name": "Յարխուշտա" }
+]
+"""
+
     }
     
     router.get("api", "acronyms") { req -> Future<[Acronym]> in
